fe6ec02e94ea333203055d3abee2e6cb7fe3f8fe
Ludwig Ahrens <ludwig.ahiw@gmail.com>
2025-07-29 17:49:17 +0200

    Backtracking

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,30 @@ fn main() {
     let mut field = Field::new([SIZE_X, SIZE_Y]);
     field.place_pin([0, 1]);
     let mut blocks = vec![
+        Block::new(vec![vec![true, true, true, true, true]]),
+        Block::new(vec![vec![true, true, true], vec![false, true, false]]),
         Block::new(vec![
             vec![true, true, true],
             vec![true, true, false],
             vec![true, false, false],
+        ]),
+        Block::new(vec![
+            vec![false, true, true],
+            vec![true, true, false],
+            vec![true, false, false],
+        ]),
+        Block::new(vec![
+            vec![true, true, true],
+            vec![true, true, true],
+            vec![false, true, false],
+        ]),
+        Block::new(vec![
+            vec![true, true, true, true],
+            vec![false, false, true, false],
+        ]),
+        Block::new(vec![
+            vec![true, true, true, true],
+            vec![false, true, true, false],
         ]),
         Block::new(vec![
             vec![true, true, true, false],
@@ -18,14 +38,27 @@ fn main() {
     ];
     while blocks.len() > 0 {
         let mut b = blocks.pop().unwrap();
-        let pos_start = b.pos;
-        while !field.block_fits(&b) {
-            let incremented = b.increment_pos();
-            if !incremented {} // TODO
+        let mut tmp = false; // TODO tidy up
+        while !field.block_fits(&b) || tmp {
+            tmp = false;
+            if !b.increment() {
+                b.reset();
+                blocks.push(b);
+                b = field.pop_block().unwrap();
+                tmp = true; // this position has been tried before; increment before placing
+                //println!(
+                    //"Removed block, {}, {}",
+                    //blocks.len() + 1,
+                    //field.blocks.len()
+                //);
+                //field.print();
+            }
         }
         field.place_block(b);
+        //println!("Placed block, {}, {}", blocks.len(), field.blocks.len());
+        //field.print();
     }
-    field.print_bool();
+    field.print();
 }
 
 struct Field {
@@ -50,6 +83,7 @@ impl Field {
         for x in 0..b.dim[0] {
             for y in 0..b.dim[1] {
                 if self.blocked[b.pos[0] + x][b.pos[1] + y] && b.foundation[x][y] {
+                    //println!("Blocked at: {}, {}",b.pos[0] + x, b.pos[1] + y);
                     return false;
                 }
             }
@@ -68,6 +102,21 @@ impl Field {
         }
         self.blocks.push(b);
     }
+    pub fn pop_block(&mut self) -> Option<Block> {
+        match self.blocks.pop() {
+            None => None,
+            Some(b) => {
+                for x in 0..b.dim[0] {
+                    for y in 0..b.dim[1] {
+                        if b.foundation[x][y] {
+                            self.blocked[b.pos[0] + x][b.pos[1] + y] = false;
+                        }
+                    }
+                }
+                return Some(b);
+            }
+        }
+    }
 
     pub fn place_pin(&mut self, pos: [usize; 2]) {
         for i in 0..2 {
@@ -77,10 +126,23 @@ impl Field {
         self.blocked[pos[0]][pos[1]] = true;
     }
 
-    pub fn print_bool(&self) {
-        for row in &self.blocked {
+    pub fn print(&self) {
+        let mut table = vec![vec![String::from("/"); self.dim[1]]; self.dim[0]];
+        let mut i = 0;
+        for b in &self.blocks {
+            for x in 0..b.dim[0] {
+                for y in 0..b.dim[1] {
+                    if b.foundation[x][y] {
+                        table[b.pos[0] + x][b.pos[1] + y] = String::from(i.to_string());
+                    }
+                }
+            }
+            i += 1;
+        }
+        for row in &table {
             println!("{:?}", row);
         }
+        println!("");
     }
 }
 
@@ -100,7 +162,15 @@ impl Block {
             rot: 0,
         }
     }
-    pub fn increment_pos(&mut self) -> bool {
+    pub fn increment(&mut self) -> bool {
+        if self.increment_pos() {
+            true
+        } else {
+            return self.increment_rot();
+        }
+    }
+
+    fn increment_pos(&mut self) -> bool {
         if self.pos[1] + self.dim[1] < SIZE_Y {
             self.pos[1] += 1;
             return true;
@@ -112,10 +182,11 @@ impl Block {
         }
         false
     }
-    pub fn increment_rot(&mut self) -> bool {
+    fn increment_rot(&mut self) -> bool {
         if self.rot == 3 {
             return false;
         } else {
+            self.pos = [0, 0];
             self.rotate();
             return true;
         }
@@ -123,6 +194,9 @@ impl Block {
 
     pub fn reset(&mut self) {
         self.pos = [0, 0];
+        while self.rot != 0 {
+            self.rotate()
+        }
     }
 
     fn rotate(&mut self) {
@@ -132,5 +206,9 @@ impl Block {
         assert!(self.dim[0] == self.foundation.len());
         assert!(self.dim[1] == self.foundation[0].len());
     }
-}
 
+    //pub fn print(&self) {
+        //println!("{:?}", self.pos);
+        //println!("{:?}", self.rot);
+    //}
+}
